773f07de7cffb004fe4977898b42a5868c36d54b
Julia Németh <julia.nemeth@he-arc.ch>
2019-11-13 08:22:15 +0100

    Updated comments

--- src/theremin/theremin.ino
+++ src/theremin/theremin.ino
@@ -1,110 +1,163 @@
 /*
- Theremin à commande optique destiné à l'initiation à la micro informatique et l'électronique
+ Theremin à commande optique destiné à l'initiation à la micro informatique et l'électronique.
 
  Développé à la http://ingenierie.he-arc.ch/
 
- Haut-parleur 8 ohms connecté à D8 (via 220 ohms en série avec 10uF)
- Photo transistor "note" sur A7 (pull-up de 47kohms)
- Photo transistor "volume" (On-Off) sur A0 (pull-up de 47kohms)
+ Haut-parleur 8 ohms connecté à D8  (via 220 ohms en série avec 10uF)
+ Photo transistor "note" sur A7     (pull-up de 47kohms)
+ Photo transistor "volume"          (On-Off) sur A0 (pull-up de 47kohms)
 
 Dans le menu Outils de l'IDE :
 - Type de carte : "Arduino Nano"
-- Processeur : "ATmega328"
-- Port : "COM3" (ou autre proposé)
+- Processeur :    "ATmega328P (OldBootloader)"
+- Port :          "COM X"           (choisir le plus grand nombre proposé pour X)
 */
-#include <math.h>                 // fonctions mathématiques
 
-// Déclaration des variables
-  long frMin = 440, frMax = 1200; // fréquence minimale et maximale en Hz
-  int minLight, maxLight;         // valeurs min et max lues sur le phototransistor (PT)
-  long onOff;                     // valeur de seuil pour la commande de la note (lue par le PT)
-  
-  // Pour le vibrato
-  int perVibrato = 15, ampVibrato = 10;
-  
-  /* invFreq  : note grave (au lieu d'aigüe) quand le phototransistor A7 est couvert
-   * invOnOff : coupe le son (au lieu de l'activer) quand le phototransistor A0 est couvert
-   * mute     : coupe le son si la note est proche des extrêmes (+/- seuil)
-   * seuil    : valeur utilisée par mute
-   * snap     : joue une note de la gamme chromatique (au lieu d'une fréquence)
-   * notes[]  : tableau des fréquences des notes de la gamme chromatique */
-  bool invFreq = false, invOnOff = false, mute = true, snap = true;
-  int seuil, notes[127]; // fréquences des notes gamme chromatique
+#include <math.h>         // bibliothèques des fonctions mathématiques de base
 
-// Code exécuté à la mise sous tension ou Reset
-void setup() {
-  // Calibrage : couvrir le phototransistor A7 après le son grave, le découvrir après le son aigü
+/*
+ * VARIABLES : Déclaration des variables qui seront utilisées dans le code
+ */
+long frMin = 440;         // fréquences minimale et maximale en Hz                                        (par défaut : 440 et 1200)
+long frMax = 1200;
+
+int minLight;             // valeurs minimale et maximale lues sur le phototransistor (PT)
+int maxLight;         
+
+long onOff;               // valeur de seuil pour la commande de la note (lue par le PT)
+
+int perVibrato = 15;      // vibrato                                                                      (par défaut : 15 et 10)
+int ampVibrato = 10;
+
+bool invFreq = false;     // note grave (au lieu d'aigüe) quand le phototransistor A7 est couvert         (par défaut : false)
+bool invOnOff = false;    // coupe le son (au lieu de l'activer) quand le phototransistor A0 est couvert  (par défaut : false)
+
+bool mute = true;         // coupe le son si la note est proche des extrêmes (+/- seuil)                  (par défaut : true)
+bool snap = true;         // joue une note de la gamme chromatique (au lieu d'une fréquence)              (par défaut : true)
+
+int seuil;                // valeur utilisée par mute
+int notes[127];           // tableau des fréquences des notes de la gamme chromatique
+
+/*
+ * FONCTIONS PRINCIPALES
+ */
+ //SETUP : Exécution au démarrage ou reset
+void setup() 
+{
+  // 1. Calibrage
   calibrage();
 
-  // Le seuil ON/OFF est au milieu des 2 valeurs lues au calibrage
-  onOff = (minLight + maxLight) / 2;
+  //2. Initialisation des seuils
+  onOff = (minLight + maxLight) / 2;      // le seuil se situe au milieu des deux valeurs lues lors du calibrage
+  seuil = (maxLight - minLight) / 8;      // les valeurs proches des seuils minimum et maximum ne produiront pas de son
 
-  // Les valeurs proches de seuil de minLight et maxLight ne produiront pas de son
-  seuil = (maxLight - minLight) / 8;
-
+  //3. Initialisation des notes
   initNotes();
 }
 
-// Code exécuté le plus souvent possible
-void loop() {
-
+//LOOP : Exécution en boucle
+void loop() 
+{
   onOff = (minLight + maxLight) / 2;
   // Serial.println(onOff);
  
-  // Lecture des phototransistors
-  int lectureNote = analogRead(A7); // lecture de la hauteur de la note
-  int lectureOnOff = analogRead(A0); // lecture de la commande du volume (ON OFF)
+  //1. Récupération des valeurs envoyées par les phototranonsistors
+  int lectureNote = analogRead(A7);       // hauteur de la note
+  int lectureOnOff = analogRead(A0);      // volume du son (ON/OFF)
+  
   bool noteOn = true;
 
-  // Conversion de la lecture en fréquence
+  //2. Conversion de la lecture en fréquence
   int freq = map(lectureNote, minLight, maxLight, frMin, frMax);
 
-  // Joue la note
-  if (invFreq) freq = frMin + frMax - freq;
-  if (invOnOff)  noteOn = (lectureOnOff < onOff);   // vrai quand PT couvert
-  else           noteOn = (lectureOnOff > onOff);   // vrai quand PT découvert
-  if (snap) freq = closest(freq);                   // "arrondi" à la note la plus proche
-  if (noteOn) tone(8, freq, 10);                    // joue une fréquence
+  //3. Sortie du son
+  if (invFreq)                            // si vrai, on inverse la fréquence (initilisé à FALSE par défaut)
+  {
+    freq = frMin + frMax - freq;          
+  }
+  if (invOnOff)                           // vrai si le phototransistor est couvert  (initialisé à FALSE par défaut)
+  {
+    noteOn = (lectureOnOff < onOff);       
+  }
+  else                                    // vrai si le phototransistor est découvert
+  {
+    noteOn = (lectureOnOff > onOff);       
+  }
+  if (snap)                               // si vrai, on prend la valeur la plus proche (initialisé à TRUE par défaut)     
+  {
+    freq = closest(freq);                 
+  }
+  if (noteOn)                             // si vrai, on joue la note
+  {
+    tone(8, freq, 10);                    
+  }
 }
 
-// Calibrage
-// Au branchement, on mesure la valeur des phototransistors recouverts et decouverts
+/*
+ * FONCTIONS : fonctions annexes utilisées dans les deux fonctions principales
+ */
+// CALIBRAGE : Exécution lors du SETUP afin de calibrer les phototransistors. Au branchement, on mesure la valeur des phototransistors recouverts et decouverts
 void calibrage()
 {
-  tone(8, 440, 200);// Annonce le début du calibrage
-  delay(1200);
-  minLight = analogRead(A7); // valeur phototransistor sur A7 recouvert
+  //1. Récupération de la valeur de la lumière lorsque les phototransistors sont couverts
+  tone(8, 440, 200);              // bip sonore qui annonce le début du calibrage (La grave)
+  delay(1200);                    // délai pour couvrir les phototransistors
+  minLight = analogRead(A7);      // récupération de la valeur minimum
 
-  tone(8, 880, 200);// Annonce le début du calibrage
-  delay(1200);
-  maxLight = analogRead(A7); // valeur phototransistor sur A7 découvert
+  //2. Récupération de la valeur de la lumière lorsque les phototransistors sont découverts
+  tone(8, 880, 200);              // bip sonore qui annonce le début du calibrage (La aigu)
+  delay(1200);                    // délai pour découvrir les phototransistors
+  maxLight = analogRead(A7);      // récupération de la valeur maximum
 
-  // si le PT était d'abord découvert, on échange les valeurs
-  if (maxLight < minLight) {
+  //3. Vérification des valeurs obtenues - dans le cas où la mesure s'est faite à l'envers, on inverse les valeurs
+  if (maxLight < minLight) 
+  {
     int tmp  = minLight;
     minLight = maxLight;
     maxLight = tmp;
   }  
 }
 
-// Initialise le tableau des fréquences des notes
-// d'après http://subsynth.sourceforge.net/midinote2freq.html
-void initNotes() {
-  int a = 440;   // Fréquence du LA6 : 440 Hz
-  for (int i = 0; i < 128; i++)
+//INITIALISATION DES NOTES : Exécution lors du SETUP après la phase de CALIBRATION, initilise le tableau des fréquences de notes
+void initNotes() 
+{
+  //1. Définition de la note de base (La3 - 440Hz)
+  int a = 440;
+
+  //2. Remplissage du tableau de notes : d'après http://subsynth.sourceforge.net/midinote2freq.html
+  for (int i = 0; i < 127; i++)
+  {
     notes[i] = a * pow(2, (i - 69) / 12.);
+  }
 }
 
-// Retourne la valeur la plus proche de freq contenue dans le tableau des notes
-int closest(int freq) {
-  int lo = 0, hi = 127, mid;
+//CLOSEST : Exécution dans le LOOP, permet de trouver la fréquence la plus proche d'une des notes contenues dans le tableau
+int closest(int freq) 
+{
+  //1. Définition des variables utilisées
+  int lo = 0, hi = 126, mid;
 
-  while (hi - lo > 1) {
+  //2. Recherche de la bonne fréquence à renvoyer
+  while (hi - lo > 1) 
+  {
     mid = floor((lo + hi) / 2);
-    if (notes[mid] < freq) lo = mid ;
-    else hi = mid;
+    
+    if (notes[mid] < freq)                    // réduction de la zone de recherche sur la moitié du tableau
+    {
+      lo = mid ;                           
+    }
+    else 
+    {
+      hi = mid;
+    }
   }
 
-  if (freq - notes[lo] <= notes[hi] - freq) return notes[lo];
-  else return notes[hi];
+  if (freq - notes[lo] <= notes[hi] - freq)   // retourne la fréquence la plus proche de celle qui a été mesurée
+  {
+    return notes[lo];                     
+  }
+  else
+  {
+    return notes[hi];
+  }
 }
